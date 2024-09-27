271e014439d2df835ffa92ccaa221fc8fc9622e9
cayan701 <cayan701@gmail.com>
2024-09-28 02:12:10 +0530

    Add new files

--- cal-app/src/screens/home/index.tsx
+++ cal-app/src/screens/home/index.tsx
@@ -1,15 +1,36 @@
 import { useRef, useState } from "react";
 
 export default function Home() {
-  const canvas = useRef<HTMLCanvasElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const [isDrawaing, setIsDrwaing] = useState(false);
 
+  const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    const canvas = canvasRef.current;
+
+    if (canvas) {
+      canvas.style.background = "black";
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        ctx.beginPath();
+        ctx.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+        setIsDrwaing(true);
+      }
+    }
+  };
+
+  const stopDrawing = () => {
+    setIsDrwaing(false);
+  };
+
   return (
     <canvas
-      ref={canvas}
+      ref={canvasRef}
       id="canvas"
       className="absolute top-0 left-0 w-full h-full"
+      onMouseDown={startDrawing}
+      onMouseOut={stopDrawing}
+      onMouseUp={stopDrawing}
     />
   );
 }
